d3556c10e1801dd0feeea65af198d9a93b19a81f
Stephan <s.dumenko@icloud.com>
2022-01-09 18:12:24 +0100

    first mod

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// // const category = 'toys';
+
+// // console.log(`https:/someurl.com/${category}/5`);
+
+// // const user = "Ivan";
+
+// // alert(`Привет, ${user}`);
+
+// let user = "Stefan";
+
+// alert(`скинь денежку, ${user}`);
+
+
+console.log('arr' + "- object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2 +2 * 2 !== '8');
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && !isClose);
+
+
+
