0224022dd2a285f0e8c4b31968e88f9509b84a5e
Stephan <s.dumenko@icloud.com>
2022-01-15 23:02:07 +0100

    case Условия

--- js/script.js
+++ js/script.js
@@ -22,22 +22,38 @@
 // // и всегда в конце добавляем консоль чтобы проверить что все в порядке 
 // console.log(personalMovieDB);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('error!');
+// }
 
-const personalMovieDB = {
-        count : numberOfFilms,
-        movies :{},
-        actors : {},
-        geners : [],
-        privat : false
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('many');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num == 50) ? console.log('Ok'): console.log('Error');
+
+// const num = 50;
+// switch (num) {
+//     case 49:
+//         console.log('False');
+//         break;
+//     case 100:
+//         console.log('False');
+//         break;
+//     case 50:
+//         console.log('TRUE');
+//         break;
+//     default:
+//         console.log('TryAgain');
+//         break;
+// }
 
 
-};
-const a = prompt('Один из последних просмотреных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотреных фильмов?', ''), 
-      d = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
