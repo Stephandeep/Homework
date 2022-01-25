fb63e5fa1f84cf2c94f7aba5acf3f2144bb583f0
Stephan <s.dumenko@icloud.com>
2022-01-25 20:40:07 +0100

    Metods

--- js/script.js
+++ js/script.js
@@ -1,43 +1,55 @@
 "use strict";
 
-let num = 20;
+const str = "Test";
+const arr = [1, 2, 4];
 
-function showFirstMessage(text) {
-    console.log(text);
-     let num = 10;
-     console.log(num);
-}
-
-showFirstMessage("Hello World");
-console.log(num);
-
-function calc(a, b) {
-    return(a+b);
-}
-
-console.log(calc(4,3));
-console.log(calc(10,3));
-console.log(calc(5,6));
-
-function ret() {
-    num = 50;
-
-    //
-
-    return(num);
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
+console.log(arr.length);
 
 
 
 
 
 
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//      let num = 10;
+//      console.log(num);
+// }
+
+// showFirstMessage("Hello World");
+// console.log(num);
+
+// // function calc(a, b) {
+// //     return(a+b);
+// // }
+
+// // console.log(calc(4,3));
+// // console.log(calc(10,3));
+// // console.log(calc(5,6));
+
+// function ret() {
+//     num = 50;
+
+//     //
+
+//     return(num);
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// let logger = function() {
+
+//     console.log("Hello");
+// };
 
 
+// logger();
 
+
+// const calc = (a, b) => a + b;
 
 
 
