b466257ecc47b5f3ad2e01f40e22c972f2777430
Stephan <s.dumenko@icloud.com>
2022-04-19 08:32:14 +0200

    new html Dumenko CV

--- js/script_.js
+++ js/script_.js
@@ -1,5 +1,59 @@
 
-// let str = "some";
+// "use stcict";
+
+// // To String
+// // 1)
+
+// console.log(typeof(String(null))); 
+
+// // 2)
+
+// console.log(typeof(5 + '')); 
+
+
+// const  num = 5;
+
+// console.log("https://vk.com/catalog/" + num);
+
+// const fontSize = 26 + 'px';
+
+// // To Number
+
+// console.log(typeof(Number('4'))); 
+
+// // 2)
+// console.log(typeof(Number(+'5'))); 
+
+
+// console.log(typeof(parseInt("15px", 10))); 
+
+
+// let answ = +prompt("Hello", "");
+
+// // To Boolean
+
+// // 0, '', null, undefined, NaN;
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// // 2)
+
+// console.log(typeof(Boolean('4'))); 
+
+// // 3)
+
+// console.log(typeof(!!"44444")); 
+
+// // let str = "some";
 // let strObj = new String(str);
 
 // // console.log(typeof(str));
