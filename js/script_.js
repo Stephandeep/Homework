ca6fec3ce10b291c046c35138828d936624d526f
Stephan <s.dumenko@icloud.com>
2022-05-05 17:38:52 +0200

    new script test

--- js/script_.js
+++ js/script_.js
@@ -1,5 +1,32 @@
+'use strict';
 
-// "use stcict";
+function createCounter () {
+    let counter = 0;
+
+    const myFunction = function(){
+        counter = counter + 1;
+        return counter;
+    };
+
+    return myFunction;
+}
+
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+console.log(c1, c2, c3);
+// let number = 5; debugger
+
+// function logNumber() {
+//     let number = 4; debugger
+//     console.log(number);
+// }
+
+// number = 6;
+
+// logNumber();debugger 
 
 // // To String
 // // 1)
